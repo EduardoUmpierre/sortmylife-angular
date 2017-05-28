cbd0b883ef61ea3e624be1b46ce6a6c1a48d2e76
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2017-05-28 14:47:06 -0300

    APP component style fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
   styleUrls: ['./app.component.sass']
-=======
-  styleUrls: ['./app.component.css']
->>>>>>> a9b748eb023d0be65854fb4c913de5c5d3aef78c
 })
 export class AppComponent {
   title = 'app works!';
