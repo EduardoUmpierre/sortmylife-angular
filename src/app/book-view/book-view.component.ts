b68734267f0af63edd985d3b68cc136fcb8b0c57
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-06-05 16:55:49 -0300

    Favorite and readed button added

--- src/app/book-view/book-view.component.ts
+++ src/app/book-view/book-view.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { BooksService } from '../books.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from 'app/book';
+
+import { BooksService } from '../books.service';
+import { ReadedBooksService } from '../readed-books.service';
+import { FavoriteBooksService } from '../favorite-books.service';
 
 @Component({
   selector: 'app-book-view',
@@ -11,17 +14,23 @@ import { Book } from 'app/book';
 export class BookViewComponent implements OnInit {
     index: number;
     book: Book;
+    readed: boolean;
+    favorite: boolean;
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
-        private service: BooksService
+        private service: BooksService,
+        private readedBooksService: ReadedBooksService,
+        private favoriteBooksService: FavoriteBooksService
     ) { }
 
     ngOnInit() {
         this.route.params.subscribe(params => {
             this.index = params['index'];
             this.book = this.service.getOneByIndex(this.index - 1);
+            this.readed = this.readedBooksService.getOneByObject(this.book);
+            this.favorite = this.favoriteBooksService.getOneByObject(this.book);
         });
     }
 
@@ -29,9 +38,32 @@ export class BookViewComponent implements OnInit {
         this.router.navigate(['livro/editar', index]);
     }
 
+    toggleReaded() {
+        if (this.readed) {
+            this.readedBooksService.delete(this.book);
+        } else {
+            this.readedBooksService.add(this.book);
+        }
+
+        this.readed = !this.readed;
+    }
+
+    toggleFavorite() {
+        if (this.favorite) {
+            this.favoriteBooksService.delete(this.book);
+        } else {
+            this.favoriteBooksService.add(this.book);
+        }
+
+        this.favorite = !this.favorite;
+    }
+
     delete(index: number) {
         if (this.confirm()) {
             this.service.delete(index);
+            this.readedBooksService.delete(this.book);
+            this.favoriteBooksService.delete(this.book);
+
             this.router.navigate(['inicio']);
         }
     }
