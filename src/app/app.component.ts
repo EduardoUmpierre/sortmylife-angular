ba8bb596807267c166d3d38d3cc564a1de854fb1
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2017-06-05 01:36:37 -0300

    Container application and book list added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Book } from 'app/book';
+import { BooksService } from 'app/books.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
+    books: Book[];
+    title = 'app works!';
+
+    constructor (private booksService: BooksService) {
+        this.books = this.booksService.getAll();
+    }
 }
