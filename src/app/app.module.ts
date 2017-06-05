983d073bf58fada1873b65a9ee74e2ed3a70d112
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-06-05 16:54:49 -0300

    Home route updated and providers added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { BooksService } from './books.service';
+import { FavoriteBooksService } from './favorite-books.service';
+import { ReadedBooksService } from './readed-books.service';
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { RouterModule, Routes } from '@angular/router';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookViewComponent } from './book-view/book-view.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoute: Routes = [
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
-    {path: 'inicio', component: BookListComponent},
+    {path: 'inicio', component: HomeComponent},
     {path: 'livro/novo', component: BookFormComponent},
     {path: 'livro/editar/:index', component: BookFormComponent},
     {path: 'livro/:index', component: BookViewComponent},
@@ -25,7 +29,8 @@ const appRoute: Routes = [
         HeaderComponent,
         BookListComponent,
         BookFormComponent,
-        BookViewComponent
+        BookViewComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
@@ -33,7 +38,11 @@ const appRoute: Routes = [
         HttpModule,
         RouterModule.forRoot(appRoute),
     ],
-    providers: [BooksService],
+    providers: [
+        BooksService,
+        FavoriteBooksService,
+        ReadedBooksService,
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
