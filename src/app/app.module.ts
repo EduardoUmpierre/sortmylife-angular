f0ab826f3c1367a5526d505290a6f9966f2b256c
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2017-06-05 01:35:32 -0300

    Book CRUD route added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BooksService } from './books.service';
+import { HeaderComponent } from './header/header.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BookFormComponent } from './book-form/book-form.component';
+import { BookViewComponent } from './book-view/book-view.component';
+
+const appRoute: Routes = [
+    {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+    {path: 'inicio', component: BookListComponent},
+    {path: 'livro/novo', component: BookFormComponent},
+    {path: 'livro/editar/:index', component: BookFormComponent},
+    {path: 'livro/:index', component: BookViewComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        BookListComponent,
+        BookFormComponent,
+        BookViewComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoute),
+    ],
+    providers: [BooksService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
